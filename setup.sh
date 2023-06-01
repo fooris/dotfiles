9e3edbcef680a38266207fd8b2bf10f1bdcf09bb
Floris Westermann <westermann.floris@gmail.com>
2023-06-01 18:00:50 +0200

    vscode, helix

--- setup.sh
+++ setup.sh
@@ -32,6 +32,15 @@ sudo update-desktop-database
 cd -
 ln -sf ${PWD}/.alacritty.yml ~
 
+# Install vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
+# Install Helix
+sudo dnf copr enable varlad/helix
+sudo dnf install helix
 
 # Link config files
 ln -sf $PWD/.config/* ~/.config
